785f0955d394a7b020b29ef24f1c756e84a3ec30
Aakash Kumar <aakash.kumar1980@yahoo.com>
2023-12-17 06:07:35 +0000

    updated

--- ui-react_app/src/components/viewport/footer/Footer.tsx
+++ ui-react_app/src/components/viewport/footer/Footer.tsx
@@ -5,7 +5,39 @@ function Footer() {
 
   return (
     <div id="footer">
-      Copywrite &copy; {year} | <a href="https://www.aaditya-designers.com/">Aaditya Designers Pvt. Ltd.</a>
+      <div style={{ width: "100vw" }} className="container">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <div className="col-md-4 d-flex align-items-center">
+            <span className="mb-3 mb-md-0 text-body-secondary">
+              © {year} Aaditya Designers Pvt. Ltd.
+            </span>
+          </div>
+          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li className="ms-3">
+              <a className="text-body-secondary" href="https://www.aaditya-designers.com/">
+                <svg className="bi" width={24} height={24}>
+                  <use xlinkHref="#twitter" />
+                </svg>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="text-body-secondary" href="https://www.aaditya-designers.com/">
+                <svg className="bi" width={24} height={24}>
+                  <use xlinkHref="#instagram" />
+                </svg>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="text-body-secondary" href="https://www.aaditya-designers.com/">
+                <svg className="bi" width={24} height={24}>
+                  <use xlinkHref="#facebook" />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </div>
+
     </div>
   );
 }
--- ui-react_app/src/components/viewport/header/Header.tsx
+++ ui-react_app/src/components/viewport/header/Header.tsx
@@ -2,11 +2,27 @@ import styles from './Header.module.scss';
 
 function Header() {
   let application_type = 'UI';
-  let application_tech = () => {return 'React App';}
+  let application_tech = () => { return 'React App'; }
 
   return (
     <div id="header" className={`${styles["header"]}`}>
-      Welcome to {application_type} {application_tech()}
+      <div id="title" style={{ textAlign: "center" }}>
+        Welcome to {application_type} {application_tech()}
+      </div>
+
+      <div style={{ width: "100vw" }} className="container">
+        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+          <div className="col-md-3 text-end">
+            <button type="button" className="btn btn-outline-danger me-2">
+              Login
+            </button>
+            <button type="button" className="btn btn-danger">
+              Sign-up
+            </button>
+          </div>
+        </header>
+      </div>
+
     </div>
   );
 }
