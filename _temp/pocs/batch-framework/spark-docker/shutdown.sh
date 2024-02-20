689cbaad611685701320f907a0c696b00af0b423
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-02-19 21:52:04 -0700

    updated

--- _temp/pocs/batch-framework/spark-docker/shutdown.sh
+++ _temp/pocs/batch-framework/spark-docker/shutdown.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+numvCPU=2
+numMemoryGB=3
+numWorkers=5
+
+executorMemoryGB=$(echo "0.75 * $numMemoryGB" | bc | awk '{print int($1+0.5)}')
+calculatedWorkerMemory=$(echo "$executorMemoryGB" | bc)
+export numvCPU numWorkers calculatedWorkerMemory
 # Stop all containers
 docker-compose down
 
--- _temp/pocs/batch-framework/spark-docker/startup.sh
+++ _temp/pocs/batch-framework/spark-docker/startup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Input variables
 numvCPU=2
 numMemoryGB=3
-numWorkers=2
+numWorkers=5
 
 executorMemoryGB=$(echo "0.75 * $numMemoryGB" | bc | awk '{print int($1+0.5)}')
 calculatedWorkerMemory=$(echo "$executorMemoryGB" | bc)
 export numvCPU numWorkers calculatedWorkerMemory
 # Start the Docker Compose process
 echo "Starting DockerCompose with $numvCPU vCPU, $calculatedWorkerMemory GB memory, and $numWorkers workers"
-docker-compose up
-
-
+docker-compose up 
+# docker-compose up -d --scale spark-worker=$numWorkers
