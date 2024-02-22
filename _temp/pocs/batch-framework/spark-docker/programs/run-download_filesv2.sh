b4439153f035d49a10cc34ef3c8a03414fa728b9
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-02-22 00:07:00 -0700

    updated

---
+++ _temp/pocs/batch-framework/spark-docker/programs/run-download_filesv2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###############################
+##### EXECUTE THE PROGRAM #####
+###############################
+# copy the program file to the master node
+docker exec spark-docker-spark-master-1 bash -c '[ -e /var/poc-workspace/download_filesv2.py ] && rm -rf /var/poc-workspace/download_filesv2.py || echo "File does not exist, nothing to remove."'
+docker cp ./download_filesv2.py spark-docker-spark-master-1:/var/poc-workspace
+# next, we will run the program to download the files from S3 to HDFS
+docker exec spark-docker-spark-master-1 /bin/bash -c "spark-submit \
+    --master spark://spark-master:7077 \
+    --conf spark.executorEnv.PYSPARK_PYTHON=/opt/bitnami/spark \
+    /var/poc-workspace/download_filesv2.py"
+
+
+### VERIFICATION ###
+# check the HDFS directory to see if the files were downloaded
+docker exec spark-docker-namenode-1 /bin/bash -c "hadoop fs -ls /poc/file-download"
+
+docker exec spark-docker-namenode-1 /bin/bash -c " \
+  hadoop fs -get /poc/file-download/dataV3.parquet/part-00000-*.snappy.parquet /tmp/temp_parquet_file.parquet"
+docker exec spark-docker-namenode-1 /bin/bash -c " \
+    java -cp /opt/parquet-tools/parquet-tools.jar:/opt/hadoop/share/hadoop/common/*:/opt/hadoop/share/hadoop/common/lib/*:/opt/hadoop/share/hadoop/mapreduce/*:/opt/hadoop/share/hadoop/mapreduce/lib/* \
+    org.apache.parquet.tools.Main head -n 3 /tmp/temp_parquet_file.parquet"
+docker exec spark-docker-namenode-1 /bin/bash -c " \
+    rm -rf /tmp/temp_parquet_file.parquet"
+        
+
