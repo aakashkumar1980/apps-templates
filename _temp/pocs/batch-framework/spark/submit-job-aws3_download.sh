67ead0dbfb27ba0cf61cfe353edb65c00bade2e0
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-04-14 20:30:37 +0000

    Update submit-job-aws3_download.sh

--- _temp/pocs/batch-framework/spark/submit-job-aws3_download.sh
+++ _temp/pocs/batch-framework/spark/submit-job-aws3_download.sh
@@ -2,42 +2,53 @@
 
 ### SPARK SUBMIT (Configuration for an example cluster environment) ###
 # Configuration of machine resources
-totalNodes=5
-corePerNode=12
+totalNodes=3
+corePerNode=16
 memoryPerNode=48 # Memory per node in GB
 
 # Optimization Rules:
-# - [per Node Level] Reserve 1 core and 1GB RAM for Hadoop/YARN/OS domains.
+# - [Per Node Level] Reserve 1 core and 1GB RAM for Hadoop/YARN/OS domains.
 # - [Cluster Level] Reserve an additional 1 core and 1GB RAM for the YARN application manager.
-# - [Executor Level] Assign 4 cores to each executor.
-# - [Executor Level] Subtract 10% of its calculated memory for memory overhead compensation.
+
 # Reservations
 reservedCoresPerNode=1    # Reserved cores per node for system
 reservedMemoryPerNode=1   # Reserved memory per node in GB for system
 reservedCoresCluster=1    # Reserved cores at cluster level for YARN Application Manager
 reservedMemoryCluster=1   # Reserved memory at cluster level in GB for YARN Application Manager
+
 # Calculations for resources available for Spark
+# Step 1: Calculate available cores and memory per node after per node reservations
 availableCoresPerNode=$(echo "$corePerNode - $reservedCoresPerNode" | bc)
-totalAvailableCores=$(echo "$totalNodes * $availableCoresPerNode - $reservedCoresCluster" | bc)
 availableMemoryPerNode=$(echo "$memoryPerNode - $reservedMemoryPerNode" | bc)
-totalAvailableMemory=$(echo "$totalNodes * $availableMemoryPerNode - $reservedMemoryCluster" | bc)
+
+# Step 2: Calculate total resources available at cluster level before YARN manager reservations
+totalCoreCluster=$(echo "$totalNodes * $availableCoresPerNode" | bc)
+totalMemoryCluster=$(echo "$totalNodes * $availableMemoryPerNode" | bc)
+
+# Step 3: Subtract cluster level reservations for YARN Application Manager
+totalAvailableCores=$(echo "$totalCoreCluster - $reservedCoresCluster" | bc)
+totalAvailableMemory=$(echo "$totalMemoryCluster - $reservedMemoryCluster" | bc)
+
 # Executor configurations
 executorCores=4   # Assigning 4 cores to each executor as per rule
-executorsPerNode=$(echo "$availableCoresPerNode / $executorCores" | bc)
-numExecutors=$(echo "$totalNodes * $executorsPerNode" | bc)  # Total executors across the cluster
-executorMemoryGB=$(echo "0.9 * ($totalAvailableMemory / $numExecutors)" | bc | awk '{print int($1+0.5)}') # Subtracting 10% for memory overhead
+numExecutors=$(echo "$totalAvailableCores / $executorCores" | bc)  # Calculate total executors across the cluster
+# Step 4: Calculate memory allocation per executor and subtract 10% for overhead
+initialExecutorMemory=$(echo "$totalAvailableMemory / $numExecutors" | bc -l)
+executorMemoryGB=$(echo "$initialExecutorMemory * 0.9" | bc | awk '{print int($1)}') # Subtracting 10% for memory overhead
+memoryOverheadMB=$(echo "$initialExecutorMemory * 0.1" | bc | awk '{print int($1 * 1024)}') # Calculating memory overhead in MB
 
 spark-submit --class com.aadityadesigners.poc.s3filesdownload.S3FileDownloadApp \
   --master spark://ip-172-31-7-170.us-west-1.compute.internal:7077 \
   --num-executors $numExecutors \
   --executor-cores $executorCores \
   --executor-memory ${executorMemoryGB}G \
-  --conf spark.executor.memoryOverhead=$(echo "0.1 * $executorMemoryGB" | bc | awk '{print int($1+0.5)}')M \
+  --conf spark.executor.memoryOverhead=${memoryOverheadMB}M \
   --conf spark.dynamicAllocation.enabled=false \
   --conf spark.default.parallelism=$((numExecutors * executorCores)) \
   --conf spark.sql.shuffle.partitions=$((numExecutors * executorCores)) \
   --driver-memory 2G \
   build/libs/app-1.0.0.jar
+
 
 
 
