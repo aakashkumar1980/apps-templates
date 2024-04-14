849d5533420ff3a5206ad6cab6523aaa4f339e92
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-04-14 19:44:15 +0000

    Update submit-job-aws3_download.sh

--- _temp/pocs/batch-framework/spark/submit-job-aws3_download.sh
+++ _temp/pocs/batch-framework/spark/submit-job-aws3_download.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # Spark Submit (TEST ON: M6A.8XLARGE | 32 vCPU | 128 GB Memory | 12.5 Gibps [1.56 GBps] Network)
+
+# Configuration of machine resources
+totalNodes=5
+corePerNode=12
+memoryPerNode=48 # Memory per node in GB
+
 # Optimization Rules:
 # - [per Node Level] Reserve 1 core and 1GB RAM for Hadoop/YARN/OS domains.
 # - [Cluster Level] Reserve an additional 1 core and 1GB RAM for the YARN application manager.
 # - [Executor Level] Assign 4 cores to each executor.
-# - [Executor Level] Subtract 10% of calculated memory for overhead.
-
+# - [Executor Level] Subtract 10% of its calculated memory for memory overhead compensation.
 # Reservations
 reservedCoresPerNode=1    # Reserved cores per node for system
 reservedMemoryPerNode=1   # Reserved memory per node in GB for system
 reservedCoresCluster=1    # Reserved cores at cluster level for YARN Application Manager
 reservedMemoryCluster=1   # Reserved memory at cluster level in GB for YARN Application Manager
-
 # Calculations for resources available for Spark
-availableCores=$(echo "32 - $reservedCoresPerNode - $reservedCoresCluster" | bc)
-availableMemory=$(echo "128 - $reservedMemoryPerNode - $reservedMemoryCluster" | bc)
+availableCoresPerNode=$(echo "$corePerNode - $reservedCoresPerNode" | bc)
+totalAvailableCores=$(echo "$totalNodes * $availableCoresPerNode - $reservedCoresCluster" | bc)
+availableMemoryPerNode=$(echo "$memoryPerNode - $reservedMemoryPerNode" | bc)
+totalAvailableMemory=$(echo "$totalNodes * $availableMemoryPerNode - $reservedMemoryCluster" | bc)
 # Executor configurations
 numWorkers=6
 executorCores=4   # Assigning 4 cores to each executor as per rule
-executorMemoryGB=$(echo "0.9 * ($availableMemory / $numWorkers)" | bc | awk '{print int($1+0.5)}') # Subtracting 10% for memory overhead
+executorMemoryGB=$(echo "0.9 * ($totalAvailableMemory / $numWorkers)" | bc | awk '{print int($1+0.5)}') # Subtracting 10% for memory overhead
 
 spark-submit --class com.aadityadesigners.poc.s3filesdownload.S3FileDownloadApp \
   --master spark://ip-172-31-7-170.us-west-1.compute.internal:7077 \
@@ -32,6 +40,8 @@ spark-submit --class com.aadityadesigners.poc.s3filesdownload.S3FileDownloadApp 
   --conf spark.sql.shuffle.partitions=$((numWorkers * executorCores)) \
   --driver-memory 2G \
   build/libs/app-1.0.0.jar
+
+
 
 
 # DEPRECATED #
