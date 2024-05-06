c9e5fcfebac8996546dc4de2be0f9ecf70528bc8
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-05-06 23:27:12 +0000

    updated

--- _temp/pocs/batch-framework/python-docker/main-count_linesV2.py
+++ _temp/pocs/batch-framework/python-docker/main-count_linesV2.py
@@ -2,17 +2,13 @@ import dask.dataframe as dd
 
 def count_lines(filename):
   if filename.endswith('.json'):
-    df = dd.read_json(filename, lines=True)
-  elif filename.endswith('.dat'):
-    # Assuming the file is a delimited text file (adjust parameters as needed)
-    df = dd.read_csv(filename, delimiter='\t')  # Example: Tab-delimited file
-  else:
-    # Assume it's a text file
-    df = dd.read_text(filename)
+    df = dd.read_json(filename, lines=False)
+  elif filename.endswith('.csv'):
+    df = dd.read_csv(filename, delimiter=',')
   return len(df)
 
 if __name__ == "__main__":
-  filename = "./_data/testfile.org-5GB.dat"
+  filename = "./_data/customers-100.json"
   num_lines = count_lines(filename)
   print("Total number of lines:", num_lines)
 
