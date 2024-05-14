9df28ac7268763ccf9258a2d52bbd18b254557ab
Aakash Kumar <aakash.kumar1980@yahoo.com>
2024-05-14 23:01:13 +0000

    updated

---
+++ _temp/pocs/batch-framework/python-docker/FileEncryptorV1.java
@@ -0,0 +1,126 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.apache.commons.io.IOUtils;
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.bcpg.CompressionAlgorithmTags;
+import org.bouncycastle.bcpg.SymmetricKeyAlgorithmTags;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openpgp.PGPCompressedDataGenerator;
+import org.bouncycastle.openpgp.PGPEncryptedDataGenerator;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.operator.jcajce.JcePGPDataEncryptorBuilder;
+import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyKeyEncryptionMethodGenerator;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Objects;
+import java.nio.file.Paths;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class FileEncryptorV1 {
+
+  static {
+    // Add Bouncy castle to JVM
+    if (Objects.isNull(Security.getProvider(BouncyCastleProvider.PROVIDER_NAME))) {
+      Security.addProvider(new BouncyCastleProvider());
+    }
+  }
+
+  @Builder.Default
+  private int compressionAlgorithm = CompressionAlgorithmTags.ZIP;
+  @Builder.Default
+  private int symmetricKeyAlgorithm = SymmetricKeyAlgorithmTags.AES_128;
+  @Builder.Default
+  private boolean armor = true;
+  @Builder.Default
+  private boolean withIntegrityCheck = true;
+  @Builder.Default
+  private int bufferSize = 1 << 16;
+
+
+  public void encrypt(OutputStream encryptOut, InputStream clearIn, long length, InputStream publicKeyIn)
+      throws IOException, PGPException {
+    PGPCompressedDataGenerator compressedDataGenerator =
+        new PGPCompressedDataGenerator(compressionAlgorithm);
+    PGPEncryptedDataGenerator pgpEncryptedDataGenerator = new PGPEncryptedDataGenerator(
+        // This bit here configures the encrypted data generator
+        new JcePGPDataEncryptorBuilder(symmetricKeyAlgorithm)
+            .setWithIntegrityPacket(withIntegrityCheck)
+            .setSecureRandom(new SecureRandom())
+            .setProvider(BouncyCastleProvider.PROVIDER_NAME)
+    );
+    // Adding public key
+    pgpEncryptedDataGenerator.addMethod(new JcePublicKeyKeyEncryptionMethodGenerator(
+        FileEncryptionDecryptionCommonUtilsV1.getPublicKey(publicKeyIn)));
+    if (armor) {
+      encryptOut = new ArmoredOutputStream(encryptOut);
+    }
+    OutputStream cipherOutStream = pgpEncryptedDataGenerator.open(encryptOut, new byte[bufferSize]);
+    FileEncryptionDecryptionCommonUtilsV1.copyAsLiteralData(compressedDataGenerator.open(cipherOutStream), clearIn, length, bufferSize);
+    // Closing all output streams in sequence
+    compressedDataGenerator.close();
+    cipherOutStream.close();
+    encryptOut.close();
+  }
+
+  public byte[] encrypt(byte[] clearData, InputStream pubicKeyIn) throws PGPException, IOException {
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(clearData);
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    encrypt(outputStream, inputStream, clearData.length, pubicKeyIn);
+    return outputStream.toByteArray();
+  }
+
+  public InputStream encrypt(InputStream clearIn, long length, InputStream publicKeyIn)
+      throws IOException, PGPException {
+    File tempFile = File.createTempFile("pgp-", "-encrypted");
+    encrypt(Files.newOutputStream(tempFile.toPath()), clearIn, length, publicKeyIn);
+    return Files.newInputStream(tempFile.toPath());
+  }
+
+  public byte[] encrypt(byte[] clearData, String publicKeyStr) throws PGPException, IOException {
+    return encrypt(clearData, IOUtils.toInputStream(publicKeyStr, Charset.defaultCharset()));
+  }
+
+  public InputStream encrypt(InputStream clearIn, long length, String publicKeyStr) throws IOException, PGPException {
+    return encrypt(clearIn, length, IOUtils.toInputStream(publicKeyStr, Charset.defaultCharset()));
+  }
+
+
+  public static void main(String[] args) {
+    //String inputFilePath = "/mnt/ebs_volume/tmp/_data/customers-256000000.csv";
+    String inputFilePath = "/mnt/ebs_volume/PrivateLearningV2.1/apps/apps-templates/_temp/pocs/batch-framework/python-docker/_data/sample.csv";
+    String publicKeyFilePath = "/mnt/ebs_volume/tmp/_data/pgp_public_key.asc";
+    String outputFilePath = inputFilePath+".pgp";
+
+    FileEncryptorV1 fileEncryptorV1 = FileEncryptorV1.builder()
+        .armor(true)
+        .compressionAlgorithm(CompressionAlgorithmTags.ZIP)
+        .symmetricKeyAlgorithm(SymmetricKeyAlgorithmTags.AES_128)
+        .withIntegrityCheck(true)
+        .build();
+
+    // start file encryption
+    try (InputStream clearIn = Files.newInputStream(Paths.get(inputFilePath));
+         InputStream publicKeyIn = Files.newInputStream(Paths.get(publicKeyFilePath))) {
+      try (OutputStream encryptOut = Files.newOutputStream(Paths.get(outputFilePath))) {
+        fileEncryptorV1.encrypt(encryptOut, clearIn, Files.size(Paths.get(inputFilePath)), publicKeyIn);
+      }
+    } catch (IOException | PGPException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
